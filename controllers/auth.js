11c0f65f3ab5fb543bca01fc869f1e84942e8f16
Sunjang1009 <sunjang1009@gmail.com>
2023-05-24 21:39:11 -0400

    fixed typo

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,9 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const router = express.Router();
 
+const { createUserToken } = require("../middleware/auth");
+
+
 router.post("/register", async (req,res,next)=>{
     try{
 
@@ -25,9 +28,17 @@ router.post("/register", async (req,res,next)=>{
 
 router.post("/login", async (req,res,next)=>{
     try{
+        const loggingUser = req.body.username;
+        const foundUser = await User.findOne({username:loggingUser});
+        const token = await createUserToken(req, foundUser);
+        res.status(200).json({
+            user:foundUser,
+            isLoggedIn:true,
+            token,
+        });
 
     }catch(err){
-        console.log(err);
+        res.status(401).json({ error: err.message })
     }
 });
 
